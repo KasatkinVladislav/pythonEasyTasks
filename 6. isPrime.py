d4f7868ef1f58c5805f3de867fce7ef8ccd19a4f
KasatkinVladislav <captain.obvious1995@yandex.ru>
2022-08-15 11:15:57 +0300

    added fifth task

---
+++ 6. isPrime.py
@@ -0,0 +1,11 @@
+def bankDeposit(amountMoney, years): #this function calculates sum of money on a bank deposit
+    sum = amountMoney
+    for oneYear in range(years):
+        sum = sum * 1.1
+    return sum
+
+print("input amount of money")
+amountMoney = float(input())
+print("input count of years")
+years = int(input())
+print(bankDeposit(amountMoney, years))
